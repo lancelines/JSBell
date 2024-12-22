4548d50d0da53921ef530be75d8aa0e656872d22
chadixearth <richard.legaspi.2003@outlook.com>
2024-12-22 19:54:10 +0800

    Add build.sh for Render deployment

--- build.sh
+++ build.sh
@@ -2,14 +2,7 @@
 # exit on error
 set -o errexit
 
-# Install python dependencies
 pip install -r requirements.txt
 
-# Create media directory
-python tailwind_django/create_media_dir.py
-
-# Collect static files
-python tailwind_django/manage.py collectstatic --no-input
-
-# Run migrations
-python tailwind_django/manage.py migrate
+python manage.py collectstatic --no-input
+python manage.py migrate
